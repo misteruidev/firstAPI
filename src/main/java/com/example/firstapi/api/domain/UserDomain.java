c11b47a4212738d95e2a884a72b816ba6237c9f3
azam <azam.uidev@gmail.com>
2023-05-23 14:32:56 +0500

    add strategy for Id

--- src/main/java/com/example/firstapi/api/domain/UserDomain.java
+++ src/main/java/com/example/firstapi/api/domain/UserDomain.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "users")
 public class UserDomain {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
